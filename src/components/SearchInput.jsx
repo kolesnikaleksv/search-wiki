3f1447528f64d29dc57ab679d00b0a418a309b2f
kolesnikaleksv <kolesnikaleksv@gmail.com>
2024-04-12 08:21:29 +0300

    Add optimization to search hook and axios extra requests

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -4,24 +4,19 @@ import { useSearch } from '../hooks/useSearch';
 
 export default function SearchInput() {
   const [value, setValue] = useState(null);
-  const {articles, status, error} = useSearch(value);
+  const {articles} = useSearch(value);
   
   return (
-    <>
-      <p>Status: {status}</p>
-      <p>Error: {error}</p>
-
-      <Autocomplete
-        disablePortal
-        id="combo-box-demo"
-        options={articles}
-        isOptionEqualToValue={(option, value) => option.value === value.value}
-        sx={{ width: 300, my: "20px"}}
-        onInputChange={(event, newInputValue) => {
-          setValue(newInputValue);
-        }}
-        renderInput={(params) => <TextField {...params} label="Wikipedia" />}
-      />
-    </>
+    <Autocomplete
+      disablePortal
+      id="combo-box-demo"
+      options={articles}
+      isOptionEqualToValue={(option, value) => option.value === value.value}
+      sx={{ width: 300, my: "20px"}}
+      onInputChange={(event, newInputValue) => {
+        setValue(newInputValue);
+      }}
+      renderInput={(params) => <TextField {...params} label="Wikipedia" />}
+    />
   );
 }
--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -1,43 +1,56 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from 'axios';
 
 export const useSearch = (query) => {
+
   const [state, setState] = useState({
     articles: [],
     status: "Idle",
     error: ''
   });
 
+  const cancelTocken = useRef(null);
+
   useEffect(() => {
-    if(!query) {
+
+    if(!query || query.length < 3) {
       return
-    } else {
-      axios.get(`https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=${query}`)
-      .then(function (response) {
-        const parsedResponce = [];
-        for(let i=0; i < response.data[1].length; i++) {
-          parsedResponce.push({
-            label: response.data[1][i],
-            url: response.data[3][i]
-          })
-        }
-        setState({
-          articles: parsedResponce,
-          status: "Success",
-          error: ''
+    }
+
+    if(cancelTocken.current) {
+      cancelTocken.current.cancel();
+    }
+    
+    cancelTocken.current = axios.CancelToken.source();
+
+    axios.get(`https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=${query}`, {
+      cancelToken: cancelTocken.current.token
+    })
+    .then(function (response) {
+      const parsedResponce = [];
+      for(let i=0; i < response.data[1].length; i++) {
+        parsedResponce.push({
+          label: response.data[1][i],
+          url: response.data[3][i]
         })
+      }
+      setState({
+        articles: parsedResponce,
+        status: "Success",
+        error: ''
       })
-      .catch(function (error) {
+    })
+    .catch(function (error) {
+      if(axios.isCancel(error)) {
+        console.log("Request canceled")
+      } else {
         setState({
           articles: [],
           status: "Error",
           error: error
         })
-      })
-      .finally(function () {
-        // always executed
+      }
     });
-    }
   },[query]);
 
   return state;
