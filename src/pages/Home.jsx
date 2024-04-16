1574036780b7b6f1d5dfde64d4956305fa116f0c
kolesnikaleksv <kolesnikaleksv@gmail.com>
2024-04-16 18:31:58 +0300

    Clean up the Search and Home components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,12 @@
 import Navigation from "../components/Navigation";
-// import SearchInput from "../components/searchInput/SearchInput";
 import SearchBox from "../components/SearchBox"
 import './home.scss';
+
 const Home = () => {
   return (
     <div>
       <Navigation />
       <div className="container">
-        {/* <SearchInput /> */}
         <SearchBox />
         Home page
       </div>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -15,26 +15,15 @@ const Search = () => {
       <Container>
         <p>Search query: {query}</p>
         <ul style={{listStyle: 'none', padding: 0}}>
-          <ListItem />
-          {/* {
+          {
             articles.length ?
             articles.map((article, i) => {
               return (
-                <li key={i}><a href={article.url} target="_blank" rel="noreferrer">{article.label}</a></li>
+                <ListItem key={i} article={article} />
               )
             })
             : <h3>There are no articles for {query}</h3>
-          } */}
-            {
-              articles.length ?
-              articles.map((article, i) => {
-                return (
-                  <ListItem key={i} article={article} />
-                  // <li key={i}><a href={article.url} target="_blank" rel="noreferrer">{article.label}</a></li>
-                )
-              })
-              : <h3>There are no articles for {query}</h3>
-            }
+          }
         </ul>
       </Container>
     </div>
